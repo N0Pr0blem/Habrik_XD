ae65130a54a105009be5703259af67779c5bc5b2
n0pr0blem <k.bobkov1408@gmail.com>
2025-04-09 21:55:10 +0300

    new: Добавил инкремент id

--- user-service/src/main/java/com/example/user_service/repository/base/SimpleListRepositoryImpl.java
+++ user-service/src/main/java/com/example/user_service/repository/base/SimpleListRepositoryImpl.java
@@ -3,15 +3,20 @@ package com.example.user_service.repository.base;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.BiConsumer;
 import java.util.function.Function;
 
 public class SimpleListRepositoryImpl<T, I> implements SimpleListRepository<T, I> {
 
     private List<T> tList = new ArrayList<>();
+    private final AtomicInteger idCounter = new AtomicInteger(1);
     private final Function<T, I> idExtractor;
+    private final BiConsumer<T, I> idSetter;
 
-    public SimpleListRepositoryImpl(Function<T, I> idExtractor) {
+    public SimpleListRepositoryImpl(Function<T, I> idExtractor,BiConsumer<T, I> idSetter) {
         this.idExtractor = idExtractor;
+        this.idSetter = idSetter;
     }
 
     @Override
@@ -28,6 +33,10 @@ public class SimpleListRepositoryImpl<T, I> implements SimpleListRepository<T, I
 
     @Override
     public T save(T t) {
+        if (idExtractor.apply(t) == null) {
+            I newId = (I) Long.valueOf(idCounter.getAndIncrement());
+            idSetter.accept(t, newId);
+        }
         tList.add(t);
         return t;
     }
