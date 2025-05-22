ea80fcde60dbbf8f804b68780d0e8ca5399b4b77
n0pr0blem <k.bobkov1408@gmail.com>
2025-05-23 00:33:04 +0300

    new: Написал простой скрипт для ребилда проекта

---
+++ bash/start_project.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+
+set -e
+
+cd ..
+
+echo "Stoping containers..."
+sudo docker-compose stop
+
+echo "Starting Maven build..."
+mvn clean install -T 1C
+
+echo "Starting containers..."
+sudo docker-compose up --build
