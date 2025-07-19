e69e9d6aacb968ad2d5b32c4cc1badb84ef0e342
n0pr0blem <k.bobkov1408@gmail.com>
2025-07-19 17:30:48 +0300

    update: get image work

--- image-service/src/main/java/com/image_service/image_service/controller/ImageController.java
+++ image-service/src/main/java/com/image_service/image_service/controller/ImageController.java
@@ -1,6 +1,7 @@
 package com.image_service.image_service.controller;
 
 import com.image_service.image_service.dto.SaveImageDto;
+import com.image_service.image_service.model.ImageFolderType;
 import com.image_service.image_service.service.ImageService;
 import io.minio.errors.*;
 import io.swagger.v3.oas.annotations.Operation;
@@ -28,6 +29,12 @@ public class ImageController {
             @RequestPart("multipartFile") MultipartFile file
     ) throws ServerException, InsufficientDataException, ErrorResponseException, NoSuchAlgorithmException, IOException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException {
         return imageService.save(imageFolderType, username, file);
+    }
+
+    @GetMapping()
+    @Operation(description = "Get image")
+    public byte[] getImage(@RequestParam String path) throws Exception{
+        return imageService.get(path);
     }
 
 }
--- image-service/src/main/java/com/image_service/image_service/service/impl/ImageServiceImpl.java
+++ image-service/src/main/java/com/image_service/image_service/service/impl/ImageServiceImpl.java
@@ -2,11 +2,13 @@ package com.image_service.image_service.service.impl;
 
 import com.image_service.image_service.model.ImageFolderType;
 import com.image_service.image_service.service.ImageService;
+import io.minio.GetObjectArgs;
 import io.minio.MinioClient;
 import io.minio.PutObjectArgs;
 import io.minio.errors.*;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import org.apache.commons.compress.compressors.FileNameUtil;
 import org.apache.commons.compress.utils.FileNameUtils;
 import org.apache.log4j.LogManager;
@@ -17,6 +19,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.util.UUID;
@@ -37,11 +40,11 @@ public class ImageServiceImpl implements ImageService {
     public String save(String folder, String subfolder, MultipartFile image) throws ServerException, InsufficientDataException, ErrorResponseException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException, IOException {
         logger.info("---Request to upload image---");
         String extension = FileNameUtils.getExtension(image.getOriginalFilename());
-        logger.info("- extension: "+extension);
-        String fileName = UUID.randomUUID()+"."+extension;
-        logger.info("- filename: "+fileName);
-        String path = folder+"/"+subfolder+"/"+fileName;
-        logger.info("- path: "+path);
+        logger.info("- extension: " + extension);
+        String fileName = UUID.randomUUID() + "." + extension;
+        logger.info("- filename: " + fileName);
+        String path = folder + "/" + subfolder + "/" + fileName;
+        logger.info("- path: " + path);
 
         if (!image.getContentType().startsWith("image/")) {
             logger.error("---Error--- ");
@@ -55,13 +58,20 @@ public class ImageServiceImpl implements ImageService {
                 .stream(image.getInputStream(), image.getSize(), -1)
                 .build());
 
-        logger.info("---Success---: "+path);
+        logger.info("---Success---: " + path);
         return path;
     }
 
+    @SneakyThrows
     @Override
     public byte[] get(String path) {
-        return new byte[0];
+        try(InputStream stream = minioClient.getObject(GetObjectArgs.builder()
+                .bucket(bucketName)
+                .object(path)
+                .build())){
+
+            return stream.readAllBytes();
+        }
     }
 
     @Override
